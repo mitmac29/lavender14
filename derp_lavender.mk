f2b4db5d24f2112bf77335a6ec2bb6a3ee0075b6
ImSpiDy <SpiDy2713@gmail.com>
2023-07-21 23:25:07 +0530

    lavender: bringup for Derpfest Android 13
    Signed-off-by: ImSpiDy <spidy2713@gmail.com>

--- derp_lavender.mk
+++ derp_lavender.mk
@@ -8,16 +8,29 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DerpFestOS stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Boot Animation
+USE_LEGACY_BOOTANIMATION := true
+
+# GMS Flags
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
+# Extra
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Product
+PRODUCT_NAME := derp_lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_lavender
 PRODUCT_MODEL := Redmi Note 7
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
