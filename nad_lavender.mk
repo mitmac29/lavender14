a9cf8f1a656e77e69c94cd96cd01fe2e07db7e7a
ImSpiDy <SpiDy2713@gmail.com>
2024-05-29 10:49:45 +0000

    lavender: bringup for Nusantara Android Q
    - users always spam for Nusa Q,
    - we got security patch update, so lets build it
    - https://review.lineageos.org/q/topic:+Q_asb_2024-04
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- nad_lavender.mk
+++ nad_lavender.mk
@@ -10,15 +10,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+NAD_BUILD_TYPE := BLU-TEST
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+PRODUCT_NAME := nad_lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_lavender
 PRODUCT_MODEL := Redmi Note 7
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
