cc41ae0e9056fa2b33e258b6df355b8bc7d90d1d
wHo-EM-i <ehteshammalik18998@gmail.com>
2024-06-14 16:18:43 +0530

    Revert "lavender: bringup for derpfest Android 13"
    This reverts commit 6430624de588f20779a8bb8449e96157408cd419.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_lavender.mk
+    $(LOCAL_DIR)/lineage_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_lavender-user \
-    derp_lavender-userdebug \
-    derp_lavender-eng
+    lineage_lavender-user \
+    lineage_lavender-userdebug \
+    lineage_lavender-eng
