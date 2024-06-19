a6bd1c1d1e137c751b704770bffd97a2ff92c49e
wHo-EM-i <ehteshammalik18998@gmail.com>
2024-06-19 12:28:39 +0530

    lavender: Welcome VoltageOS
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lavender.mk
+    $(LOCAL_DIR)/voltage_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lavender-user \
-    lineage_lavender-userdebug \
-    lineage_lavender-eng
+    voltage_lavender-user \
+    voltage_lavender-userdebug \
+    voltage_lavender-eng
