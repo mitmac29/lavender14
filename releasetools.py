1a1053f17285f9d62aae452c7f446ba2928e7946
wHo-EM-i <ehteshammalik18998@gmail.com>
2024-09-16 23:11:13 +0530

    lavender: Always flash super_image
    Instead of checking non-rdp lets just flash super image and do mapping afterwards
    
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -5,6 +5,7 @@
 #
 
 import common
+import re
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
@@ -12,16 +13,22 @@ def FullOTA_InstallEnd(info):
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
 
-def AddImage(info, basename, dest):
-  path = "IMAGES/" + basename
-  if path not in info.input_zip.namelist():
-    return
+def AddImage(info, dir, basename, dest):
+  name = basename
+  data = info.input_zip.read(dir + "/" + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
-  data = info.input_zip.read(path)
-  common.ZipWriteStr(info.output_zip, basename, data)
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+def FullOTA_InstallBegin(info):
+  info.script.AppendExtra('ifelse(is_mounted("/system_root"),unmount("/system_root"));')
+  info.script.AppendExtra('ifelse(is_mounted("/vendor"),unmount("/vendor"));')
+  info.script.AppendExtra('run_program("/system/bin/toybox", "blkdiscard", "/dev/block/bootdevice/by-name/system"); || abort("ERROR: Failed to discard data on system partition.");')
+  info.script.AppendExtra('run_program("/system/bin/toybox", "blkdiscard", "/dev/block/bootdevice/by-name/vendor"); || abort("ERROR: Failed to discard data on vendor partition.");')
+  info.script.AppendExtra('ui_print("- Flashing super_empty onto system partition...");')
+  AddImage(info, "RADIO", "super_dummy.img", "/dev/block/bootdevice/by-name/system");
+  return
 
 def OTA_InstallEnd(info):
   info.script.Print("Patching device-tree and verity images...")
-  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  AddImage(info, "IMAGES", "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, "IMAGES", "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
