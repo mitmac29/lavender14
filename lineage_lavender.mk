c9e0503d6d213ac003fdc7e59700828ac7b00420
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    lavender: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -25,6 +25,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := lavender
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V12.5.3.0.QFGCNXM release-keys"
+    BuildDesc="lavender-user 10 QKQ1.190910.002 V12.5.3.0.QFGCNXM release-keys"
 
 BUILD_FINGERPRINT := xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.5.3.0.QFGCNXM:user/release-keys
