8ee8ffaf81042745ac8dc3a534327e661e75f0c6
wHo-EM-i <ehteshammalik18998@gmail.com>
2024-10-23 17:40:03 +0530

    lavender: RemovePackages: update list
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -29,6 +29,9 @@ LOCAL_OVERRIDES_PACKAGES := \
 	Maps \
 	MyVerizonServices \
 	PixelLiveWallpaperPrebuilt \
+	PixelSetupWizardOverlay \
+	PixelSetupWizardOverlay2021 \
+	PixelSetupWizardStringsOverlay \
 	Gmail \
 	Gmail2 \
 	PrebuiltGmail \
@@ -61,8 +64,10 @@ LOCAL_OVERRIDES_PACKAGES := \
 	VZWAPNLib \
 	VzwOmaTrigger \
 	WallpapersBReel2020 \
+	Wellbeing \
 	YouTube \
 	YouTubeMusicPrebuilt \
+	SetupWizardPixelPrebuilt \
 	TurboPrebuilt \
 	TagGoogle \
 	talkback \
