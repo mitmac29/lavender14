6d75140d6aec6820f67efab6c843516e62815027
ImSpiDy <SpiDy2713@gmail.com>
2024-11-14 21:35:22 +0530

    lavender: Set TARGET_KERNEL_VERSION = 4.19
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- aospa_lavender.mk
+++ aospa_lavender.mk
@@ -11,6 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common aospa stuff
 $(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
 WITH_GMS := true
+TARGET_KERNEL_VERSION := 4.19
 
 # Bootanimation resolution
 TARGET_BOOT_ANIMATION_RES := 1080
