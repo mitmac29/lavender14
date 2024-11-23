413a112edff0ddeca893c43b23ac87eef4248c6d
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-11-23 22:56:01 +0530

    Update and rename lineage_lavender.mk to aicp_lavender.mk

--- aicp_lavender.mk
+++ aicp_lavender.mk
@@ -8,21 +8,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Aicp stuff
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-#RisingOS
-TARGET_ENABLE_BLUR := false
-PRODUCT_NO_CAMERA := false
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
-
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_lavender
+PRODUCT_NAME := aicp_lavender
 PRODUCT_MODEL := Redmi Note 7
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
